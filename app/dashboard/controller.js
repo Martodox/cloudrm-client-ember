9f14e35fb80152e6459fba2b30a95a48a50b5847
Kinga Biernacka <kingabiernacka@op.pl>
2018-01-03 22:30:40 +0100

    styles added

--- app/dashboard/controller.js
+++ app/dashboard/controller.js
@@ -1,9 +1,13 @@
 import Controller from '@ember/controller';
 
 export default Controller.extend({
+  isMenuOpen: false,
   actions: {
     logout() {
       this.send('destroySession');
+    },
+    toggleNavBar() {
+      this.toggleProperty("isMenuOpen")
     }
   }
 });
